62ed72530a1f9b6f4550c8838607cf6766f8e899
Chasebyui22 <smi19037@byui.edu>
2024-10-19 15:15:30 -0600

    fix: Move requirements.txt to backend directory

--- trash/app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class CanvasUser(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    role = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
